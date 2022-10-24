0e561e5786cd1db77eb7d22be2ec2c55e27f9a14
pwolf15 <patrick.wolf@live.com>
2022-10-23 21:22:41 -0700

    Cleaned up transforms

--- examples/common/transforms.rs
+++ examples/common/transforms.rs
@@ -13,24 +13,3 @@ pub fn create_transforms(translation:[f32; 3], rotation:[f32; 3], scaling:[f32; 
 
   model_mat
 }
-
-fn main() {
-  let my_vec = Vector4::new(1.0, 2.0, 3.0, 1.0);
-
-  let rot_mat_z = Matrix4::from_angle_z(Rad( 20.0 * PI / 180.0));
-
-  let rot_mat = rot_mat_z * Matrix4::from_angle_z(Rad(25.0 * PI / 180.0 ));
-
-  let rot_vec = rot_mat * my_vec;
-
-  println!("\nOriginal vector: \n{:?}", my_vec);
-  println!(
-    "Total rotation matrix after two rotations: rot_mat: \n{:?}", 
-    rot_mat
-  );
-
-  println!(
-    "Vector after two translations: rot_vec = rot_mat * my_vec = \n{:?}\n", 
-    rot_vec
-  );
-}
