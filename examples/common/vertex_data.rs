6c6fffc44e2e358518e27d7959fdd50cd2ea504e
pwolf15 <patrick.wolf@live.com>
2022-12-29 17:17:50 -0800

    Added 'cube_vertex_color'

--- examples/common/vertex_data.rs
+++ examples/common/vertex_data.rs
@@ -165,3 +165,38 @@ pub fn cube_data() -> (Vec<[i8; 3]>, Vec<[i8; 3]>, Vec<[i8; 2]>, Vec<[i8; 3]>) {
 
   (positions.to_vec(), colors.to_vec(), uvs.to_vec(), normals.to_vec())
 }
+
+pub fn cube_data_index() -> (Vec<[i8; 3]>, Vec<[i8; 3]>, Vec<u16>) {
+    let positions = [
+        [-1,-1,1],
+        [1,-1,1],
+        [1,1,1],
+        [-1,1,1],
+        [-1,-1,-1],
+        [1,-1,-1],
+        [1,1,-1],
+        [-1,1,-1],
+    ];
+
+    let colors = [
+        [0,0,1],
+        [1,0,1],
+        [1,1,1],
+        [0,1,1],
+        [0,0,0],
+        [1,0,0],
+        [1,1,0],
+        [0,1,0],
+    ];
+
+    let indices = [
+        0,1,2,2,3,0,
+        1,5,6,6,2,1,
+        4,7,6,6,5,4,
+        0,3,7,7,4,0,
+        3,2,6,6,7,3,
+        0,4,5,5,1,0,
+    ];
+
+    (positions.to_vec(), colors.to_vec(), indices.to_vec())
+}
