384b9f25cc58653cb858f74116f71d34ba383651
pwolf15 <patrick.wolf@live.com>
2023-01-21 23:23:20 -0800

    Updates to common

--- examples/ch07/common.rs
+++ examples/ch07/common.rs
@@ -15,7 +15,24 @@ const ANIMATION_SPEED:f32 = 1.0;
 const IS_PERSPECTIVE:bool = true;
 
 #[repr(C)]
-#[derive(COpy, Clone, Debug, Pod, Zeroables)]
+#[derive(Copy, Clone, Debug, Pod, Zeroables)]
 pub struct Vertex {
     position: [f32; 4],
+}
+
+pub fn vertex(p:[f32;3]) -> Vertex {
+    Vertex {
+        position: [p[0], p[1], p[2], 1.0],
+    }
+}
+
+impl Vertex {
+    const ATTRIBUTES: [wgpu::VertexAttribute; 2] = wgpu::vertex_attr_array![0=>Float32x4, 1=>Float32x4];
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBUTES,
+        }
+    }
 }
